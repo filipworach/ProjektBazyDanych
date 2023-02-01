8163a4bbebac1ac932a06b9ff56eefbadfcebd9a
jakulej <j.kulej01@gmail.com>
2023-02-01 20:05:16 +0100

    done

--- ProjektBazyDanych/MainWindow.xaml.cs
+++ ProjektBazyDanych/MainWindow.xaml.cs
@@ -52,7 +52,7 @@ namespace ProjektBazyDanych
                 {
                     if (login.Equals(user.user_name) && passwordd.Equals(user.password))
                     {
-                        Window1 window = new Window1(user);
+                        Window1 window = new Window1(login);
                         window.Show(); ;
                         this.Close();
                     }
